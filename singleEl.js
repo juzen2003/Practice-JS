3e17f44088e752a3b1d31556437b12d6c85a4ba6
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-02 13:01:33 -0700

    Update singleEl

--- singleEl.js
+++ singleEl.js
@@ -8,22 +8,34 @@ const singleEl = function(arr) {
 
   let mid = Math.floor(arr.length/2);
 
+
+
   let left = arr.slice(0, mid);
   let right = arr.slice(mid+1);
-  // console.log("LEFT " + left);
-  // console.log("RIGHT " + right);
+  // // console.log("LEFT " + left);
+  // // console.log("RIGHT " + right);
   if(left.includes(arr[mid])) {
-    left = left.concat([arr[mid]]);
-    console.log(left);
-    return singleEl(left);
+    // if it's even, and found the match, unique one is there
+    if (left.length % 2 === 0) {
+      left = left.concat([arr[mid]]);
+      return singleEl(left);
+    //  if it's odd, and found the match, unique is on the other side
+    } else {
+      // right = [arr[mid]].concat(right);
+      let res = singleEl(right);
+      return res + mid+1;
+    }
 
   } else if (right.includes(arr[mid])){
-    right = [arr[mid]].concat(right);
-    // console.log(right);
-    // return right;
-    let res = singleEl(right);
-    console.log(res);
-    // return res + mid;
+
+    if  (right.length % 2 === 0) {
+      right = [arr[mid]].concat(right);
+      let res = singleEl(right);
+      return res + mid;
+    } else {
+      // left = left.concat([arr[mid]]);
+      return singleEl(left);
+    }
 
   } else {
     return mid;
@@ -31,6 +43,14 @@ const singleEl = function(arr) {
 
 };
 
-let arr = [1,1,2,2,3];
-let fRes = singleEl(arr);
-console.log("FINAL " + fRes);
+let arr1 = [1,1,2,2,3];
+let fRes1 = singleEl(arr1);
+console.log("FINAL " + fRes1); // 4
+
+let arr2 = [1,1,2,3,3];
+let fRes2 = singleEl(arr2);
+console.log("FINAL " + fRes2); // 2
+
+let arr3 = [1,1,2,2,3,3,4];
+let fRes3 = singleEl(arr3);
+console.log("FINAL " + fRes3); // 6
