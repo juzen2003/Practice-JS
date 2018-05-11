03ad2198182019faf91e3576699c7177ca5387d3
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-10 21:49:25 -0700

    Add chooseARecord

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 const obj = {"a": 2, "b": 4, "c": 6}
 Object.keys(obj) // ["a", "b", "c"]
 ```
+### for...in
+```js
+let hsh = {'a': 1, 'b': 2};
+for(let a in hsh) {
+  console.log(a);
+  console.log(hsh[a]);
+}
+// a
+// 1
+// b
+// 2
+```
+### Math.random
+```js
+// random number from 0-19
+//  Math.random() return 0 to 1 (not included 1)
+Math.floor(Math.random() * 20)
+```
---
+++ chooseARecord.js
@@ -0,0 +1,33 @@
+// Write a method that takes a hash of symbol keys, for which the values are integers representing each key's weight. The method returns a key such that the chances of selecting a particular key are weighted by that key's value.
+
+// For the hash {:a => 1, :b => 2, :c => 3}, the chance of returning :c is 1/2, :b is 1/3, and :a is 1/6.
+
+
+const chooseARecord = function(hsh) {
+  let total = 0;
+  Object.keys(hsh).forEach(el => {
+    total += hsh[el];
+  });
+
+  let randomNum = getRandNum(total);
+  let sum = 0;
+  let keyArr = Object.keys(hsh);
+
+  for(let i = 0; i < keyArr.length; i++) {
+    
+    if(randomNum < hsh[keyArr[i]]) {
+      return keyArr[i];
+    }
+    total -= hsh[keyArr[i]];
+    randomNum = getRandNum(total);
+  }
+};
+
+// 0 to max -1
+const getRandNum = function(max) {
+  return Math.floor(Math.random() * (max));
+};
+
+let hsh = {'a': 1, 'b': 2, 'c': 3};
+let res = chooseARecord(hsh);
+console.log(`FINAL: ${res}`);
