ec95b50d47a3fa61a548fb10aa20e0b209c0d7cb
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-17 18:22:19 -0700

    Add longestCommonSubstring

--- longestCommonSubstring.js
+++ longestCommonSubstring.js
@@ -3,5 +3,29 @@
 // Bonus: solve it in O(m * n) using O(m * n) extra space. (Hint: the solution involves dynamic programming which will be introduced later in the course.)
 
 const longestCommonSubstring = function(str1, str2) {
-  
+  let longestSub = "";
+  let startIdx = 0;
+  while(startIdx < str1.length) {
+
+    let subLength = longestSub.length + 1;
+    while(startIdx + subLength <= str1.length) {
+      let endIdx = startIdx + subLength;
+      let subStr = str1.slice(startIdx, endIdx);
+      if(str2.includes(subStr)) {
+        if(longestSub.length < subStr.length) {
+          longestSub = subStr;
+        }
+      }
+      subLength += 1;
+    }
+
+    startIdx += 1;
+  }
+
+  return longestSub;
 };
+
+let str1 = "abcde";
+let str2 = "ghabcdegg";
+let res = longestCommonSubstring(str1, str2);
+console.log(res);
