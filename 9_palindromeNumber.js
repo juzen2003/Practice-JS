760effa651a482870f73ae7239110150ca7af7f4
Yu-Jen Chang <juzen2003@gmail.com>
2018-05-22 15:25:02 -0700

    Add palindromeNumber

---
+++ 9_palindromeNumber.js
@@ -0,0 +1,22 @@
+// Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x < 0) return false;
+  // console.log(reverseNum(x));
+  return x === reverseNum(x);
+};
+
+const reverseNum = function(x) {
+  let res = 0;
+  while(x >= 10) {
+    // console.log(res);
+    res = res * 10;
+    res += x % 10;
+    x = Math.floor(x / 10);
+  }
+
+  return res * 10 + x;
+};
