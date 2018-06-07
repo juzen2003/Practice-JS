c909e8a7d72a89b70edeef7fbac6475f45698f44
Yu-Jen Chang <juzen2003@gmail.com>
2018-06-07 00:58:05 -0700

    Update misc

--- misc/misc_random_060618.js
+++ misc/misc_random_060618.js
@@ -1,3 +1,7 @@
+// **************************
+// MISC Check
+// **************************
+// Closure
 // Refers to variables in outer scope.
 // The returned function access themessage variable from the enclosing scope.
 // It can refer to outer scope variables even after the outer function has returned.
@@ -11,5 +15,49 @@ function sayHi(name){
   return greeting;
 }
 var sayHiToJon = sayHi('Jon');
+console.log(" === closure === ");
 console.log(sayHiToJon);     // ƒ() { console.log(message) }
 console.log(sayHiToJon());   // 'Hi Jon!'
+
+
+console.log(" === IIFE === ");
+var result = [];
+for (var i=0; i < 5; i++) {
+  result.push( function() { return i; } );
+}
+console.log( result[1]() ); // 5
+console.log( result[3]() ); // 5
+result = [];
+for (var i=0; i < 5; i++) {
+  (function () {
+    var j = i; // copy current value of i
+    result.push( function() { return j; } );
+  })();
+}
+console.log( result[1]() ); // 1
+console.log( result[3]() ); // 3
+for (var i=0; i < 5; i++) {
+  (function (n) {
+    result.push( function() { return n; } );
+  })(i);
+}
+console.log( result[1]() ); // 1
+console.log( result[3]() ); // 3
+
+// call, apply, bind
+console.log(" === call, apply === ");
+const Snow = {surename: 'Snow'};
+const char = {
+  surename: 'Stark',
+  knows: function(arg, name) {
+    console.log(`You know ${arg}, ${name} ${this.surename}`);
+  }
+};
+char.knows('something', 'Bran');              // You know something, Bran Stark
+char.knows.call(Snow, 'nothing', 'Jon');      // You know nothing, Jon Snow
+// char.knows.call(Snow, ...['nothing', 'Jon']);      // You know nothing, Jon Snow
+char.knows.apply(Snow, ['nothing', 'Jon']);   // You know nothing, Jon Snow
+
+console.log(" === bind === ");
+const whoKnowsNothing = char.knows.bind(Snow, 'nothing');
+whoKnowsNothing('Jon');  // You know nothing, Jon Snow
