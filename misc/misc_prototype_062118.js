18bb344b66ca1fab5941574f9a0971d6bab76a7e
Yu-Jen Chang <juzen2003@gmail.com>
2018-06-21 17:26:30 -0700

    Update misc

--- misc/misc_prototype_062118.js
+++ misc/misc_prototype_062118.js
@@ -1,6 +1,7 @@
 // **************************
 // MISC prototype
 // **************************
+console.log("=========== Prototype ===========");
 function Dog(breed, name){
   this.breed = breed;
   this.name = name;
@@ -29,9 +30,53 @@ rusty.describe();   // "Rusty is a Beagle"
 /* the created object .__proto__ property points to the .prototype property of the constructor function */
 console.log("=== rusty.__proto__ ===");
 console.log(rusty.__proto__);    // { describe: ƒ , constructor: ƒ } same as Dog.prototype
+console.log(rusty.__proto__ === Dog.prototype);    // true
 
 /* .constructor property points to the constructor of the object */
 console.log("=== rusty.constructor ===");
 console.log(rusty.constructor);  // ƒ Dog(breed, name) { ... }
 
 console.log(Object.prototype); // {}
+
+// **************************
+// MISC Own vs Inherited Properties
+// **************************
+console.log("=========== Own vs Inherited Properties ===========");
+function Car() { }
+Car.prototype.wheels = 4;
+Car.prototype.airbags = 1;
+
+var myCar = new Car();
+myCar.color = 'black';
+
+/*  Check for Property including Prototype Chain:  */
+console.log('airbags' in myCar);  // true
+console.log(myCar.wheels);        // 4
+console.log(myCar.year);          // undefined
+
+/*  Check for Own Property:  */
+console.log(myCar.hasOwnProperty('airbags'));  // false — Inherited
+console.log(myCar.hasOwnProperty('color'));    // true
+
+// **************************
+// MISC Object.create(obj)
+// **************************
+// Object.create(obj) — Creates a new object with the specified prototype object and properties.
+console.log("=========== Object.create(obj) ===========");
+var dog = { legs: 4 };
+var myDog = Object.create(dog);
+
+console.log(myDog.hasOwnProperty('legs'));  // false
+console.log(myDog.legs);                    // 4
+console.log(myDog.__proto__ === dog);       // true
+
+console.log("=========== Inheritance by reference ===========");
+var objProt = { text: 'original' };
+var objAttachedToProt = Object.create(objProt);
+console.log(objAttachedToProt.text);   // original
+
+objProt.text = 'prototype property changed';
+console.log(objAttachedToProt.text);   // prototype property changed
+
+objProt = { text: 'replacing property' };
+console.log(objAttachedToProt.text);   // prototype property changed
