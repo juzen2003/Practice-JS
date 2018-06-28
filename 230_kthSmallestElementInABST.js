3ac704428cc4a17f99c0ab9c91fc9cdfebef2249
Yu-Jen Chang <juzen2003@gmail.com>
2018-06-27 22:39:42 -0700

    Update 230

--- 230_kthSmallestElementInABST.js
+++ 230_kthSmallestElementInABST.js
@@ -11,23 +11,19 @@
  * @return {number}
  */
 var kthSmallest = function(root, k) {
+  let count = countNodes(root.left);
 
-    let count = countNodes(root.left);
-
-    // count + 1 > k, root + counts nodes > k, it's at left, recursive running on left
-    if(count + 1 > k) {
-        return kthSmallest(root.left, k);
-    } else if (count + 1 < k) {
-        // count + 1 > k, it's at right, it's at right, recursive running on right
-        return kthSmallest(root.right, k-(count+1));
-    } else if (count + 1 === k) {
-        return root.val;
-    }
-
+  if( count + 1 === k) {
+    return root.val;
+  } else if(count + 1 > k) {
+    return kthSmallest(root.left, k);
+  } else if (count + 1 < k) {
+    return kthSmallest(root.right, k - count - 1);
+  }
 };
 
-const countNodes = function(node) {
-    if(node === null) return 0;
+const countNodes = function(root) {
+  if(root === null) return 0;
 
-    return 1 + countNodes(node.left) + countNodes(node.right);
+  return 1 + countNodes(root.left) + countNodes(root.right);
 };
