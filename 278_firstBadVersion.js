c052f3dfb7877b95a4011908062217ce3535ed8c
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-07 23:40:46 -0700

    Update 278

--- 278_firstBadVersion.js
+++ 278_firstBadVersion.js
@@ -13,23 +13,24 @@
  * @return {function}
  */
 var solution = function(isBadVersion) {
-    /**
-     * @param {integer} n Total versions
-     * @return {integer} The first bad version
-     */
-    return function(n) {
-        let low = 1;
-        let high = n;
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+    let low = 1;
+    let high = n;
 
-        while(low <= high) {
-            let mid = parseInt((low + high) / 2);
-            if(isBadVersion(mid) === false) {
-                low = mid  + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
+    while(low <= high) {
+      let mid = parseInt((low + high) / 2);
 
-        return low;
-    };
+      if(isBadVersion(mid)) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return low;
+  };
 };
