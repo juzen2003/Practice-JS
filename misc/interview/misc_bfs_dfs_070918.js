24aeabf684c5e718243c024e4c4d41a0098a152e
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-10 16:35:51 -0700

    Add misc

--- misc/interview/misc_bfs_dfs_070918.js
+++ misc/interview/misc_bfs_dfs_070918.js
@@ -23,13 +23,65 @@ let a1 = new TreeNode("a", [a2, c2]);
 // root
 let a0 = new TreeNode("a", [a1, b1, b12, c1]);
 
+const bfs_count_by_level = function(treeNode) {
+  if(treeNode.children === null) return treeNode.val;
+
+  let queue = [];
+  let currentNode;
+  let count = {};
+
+  let levelSize = 1;
+  let levelCount = 0;
+  let nextlevelSize = treeNode.children.length;
+  let visited = [];
+
+  // let visited = [];
+  queue.push(treeNode);
+
+  while(queue.length !== 0) {
+    currentNode = queue.shift();
+    // callback is normally here
+    levelCount++;
+
+    if(!visited.includes(currentNode.val)) {
+      count[currentNode.val] = count[currentNode.val] === undefined ? 1 : count[currentNode.val] += 1;
+
+      visited.push(currentNode.val);
+    }
+
+    console.log(currentNode.val);
+    // console.log(levelSize);
+    // console.log(levelCount);
+    console.log(count);
+
+    if(levelCount === levelSize) {
+      levelCount = 0;
+      levelSize = nextlevelSize;
+      visited = [];
+      nextlevelSize = 0;
+    }
+
+    if(currentNode.children === null) {
+      continue;
+    } else {
+      nextlevelSize += currentNode.children.length;
+      // console.log(`next: ${nextlevelSize}`);
+
+      for(let i = 0; i < currentNode.children.length; i++) {
+        queue.push(currentNode.children[i]);
+      }
+    }
+
+  }
+};
+
 const bfs = function(treeNode) {
   if(treeNode.children === null) return treeNode.val;
 
   let queue = [];
   let currentNode;
   let count = {};
-  // let visited = [];
+
   queue.push(treeNode);
 
   while(queue.length !== 0) {
@@ -39,6 +91,7 @@ const bfs = function(treeNode) {
     count[currentNode.val] = count[currentNode.val] === undefined ? 1 : count[currentNode.val] += 1;
 
     console.log(currentNode.val);
+    console.log(count);
 
     if(currentNode.children === null) {
       continue;
@@ -65,6 +118,7 @@ const dfs = function(treeNode) {
     count[currentNode.val] = count[currentNode.val] === undefined ? 1 : count[currentNode.val] += 1;
 
     console.log(currentNode.val);
+    console.log(count);
 
     if(currentNode.children === null) {
       continue;
@@ -76,7 +130,8 @@ const dfs = function(treeNode) {
   }
 };
 
-
+console.log("===== bfs count by level =====");
+bfs_count_by_level(a0);
 console.log("===== bfs =====");
 bfs(a0);
 console.log("===== dfs =====");
