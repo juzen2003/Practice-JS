2a8d6abb9c61cb0511d756baab47997266f3a3e4
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-13 18:33:57 -0700

    Update misc

--- misc/interview/misc_bfs_dfs_070918.js
+++ misc/interview/misc_bfs_dfs_070918.js
@@ -23,59 +23,84 @@ let a1 = new TreeNode("a", [a2, c2]);
 // root
 let a0 = new TreeNode("a", [a1, b1, b12, c1]);
 
+// const bfs_count_by_level = function(treeNode) {
+//   if(treeNode.children === null) return treeNode.val;
+//
+//   let queue = [];
+//   let currentNode;
+//   let count = {};
+//
+//   let levelSize = 1;
+//   let levelCount = 0;
+//   // let nextlevelSize = treeNode.children.length;
+//   let nextlevelSize = 0;
+//   let visited = [];
+//
+//   // let visited = [];
+//   queue.push(treeNode);
+//
+//   while(queue.length !== 0) {
+//     currentNode = queue.shift();
+//     // callback is normally here
+//     levelCount++;
+//
+//     if(!visited.includes(currentNode.val)) {
+//       count[currentNode.val] = count[currentNode.val] === undefined ? 1 : count[currentNode.val] += 1;
+//
+//       visited.push(currentNode.val);
+//     }
+//
+//     console.log(currentNode.val);
+//
+//     if(currentNode.children !== null) {
+//       nextlevelSize += currentNode.children.length;
+//
+//       for(let i = 0; i < currentNode.children.length; i++) {
+//         queue.push(currentNode.children[i]);
+//       }
+//     }
+//     // console.log(`nextlevelSize: ${nextlevelSize}`);
+//     // console.log(`levelCount: ${levelCount}`);
+//     // console.log(`levelSize: ${levelSize}`);
+//     // console.log(`visited: ${visited}`);
+//     console.log(count);
+//     // reset when finishing searching through one level
+//     if(levelCount === levelSize) {
+//       levelCount = 0;
+//       levelSize = nextlevelSize;
+//       visited = [];
+//       nextlevelSize = 0;
+//     }
+//
+//   }
+// };
 const bfs_count_by_level = function(treeNode) {
   if(treeNode.children === null) return treeNode.val;
-
   let queue = [];
-  let currentNode;
   let count = {};
-
-  let levelSize = 1;
-  let levelCount = 0;
-  // let nextlevelSize = treeNode.children.length;
-  let nextlevelSize = 0;
-  let visited = [];
-
-  // let visited = [];
   queue.push(treeNode);
 
   while(queue.length !== 0) {
-    currentNode = queue.shift();
-    // callback is normally here
-    levelCount++;
+    // reset after looping through each level
+    let size = queue.length;
+    let currentNode;
+    let visited = [];
 
-    if(!visited.includes(currentNode.val)) {
-      count[currentNode.val] = count[currentNode.val] === undefined ? 1 : count[currentNode.val] += 1;
-
-      visited.push(currentNode.val);
-    }
-
-    console.log(currentNode.val);
-    // console.log(levelSize);
-    // console.log(levelCount);
-    // console.log(count);
-
-    if(currentNode.children !== null) {
-      nextlevelSize += currentNode.children.length;
-      // console.log(`next: ${nextlevelSize}`);
-
-      for(let i = 0; i < currentNode.children.length; i++) {
-        queue.push(currentNode.children[i]);
+    // loop through each level (BFS)
+    for(let i = 0; i < size; i++) {
+      currentNode = queue.shift();
+      if(!visited.includes(currentNode.val)) {
+        count[currentNode.val] = count[currentNode.val] === undefined ? 1 : count[currentNode.val] += 1;
+        visited.push(currentNode.val);
+      }
+      console.log(currentNode.val);
+      console.log(count);
+      if(currentNode.children !== null) {
+        for(let j = 0; j < currentNode.children.length; j++) {
+          queue.push(currentNode.children[j]);
+        }
       }
     }
-    // console.log(`nextlevelSize: ${nextlevelSize}`);
-    // console.log(`levelCount: ${levelCount}`);
-    // console.log(`levelSize: ${levelSize}`);
-    // console.log(`visited: ${visited}`);
-    console.log(count);
-    // reset when finishing searching through one level
-    if(levelCount === levelSize) {
-      levelCount = 0;
-      levelSize = nextlevelSize;
-      visited = [];
-      nextlevelSize = 0;
-    }
-
   }
 };
 
