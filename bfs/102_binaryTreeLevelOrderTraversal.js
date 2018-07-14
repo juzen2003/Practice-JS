b502f140e4a09372e8fbccfec4b7205287f075b1
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-13 20:19:12 -0700

    Add 102

--- bfs/102_binaryTreeLevelOrderTraversal.js
+++ bfs/102_binaryTreeLevelOrderTraversal.js
@@ -9,6 +9,34 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+// Binary tree level order traversal
 var levelOrder = function(root) {
-    
+  if(root === null) return [];
+  let queue = [];
+  let res = [];
+  queue.push(root);
+
+  while(queue.length !== 0) {
+    let size = queue.length;
+    let levelArr = [];
+    let currentNode;
+
+    // loop through each level
+    for(let i = 0; i < size; i++) {
+      currentNode = queue.shift();
+      levelArr.push(currentNode.val);
+
+      if(currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+
+      if(currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+    }
+
+    res.push(levelArr);
+  }
+
+  return res;
 };
