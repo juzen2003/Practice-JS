1dc949fc3b7f456cf6d9a988f2ffb8d3b6d49aa8
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-15 01:35:28 -0700

    Add 103

---
+++ bfs/103_binaryTreeZigzagLevelOrderTraversal.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+    if(root === null) return [];
+    let queue = [];
+    let res = [];
+    let level = 0;
+
+    queue.push(root);
+
+    while(queue.length !== 0) {
+        let size = queue.length;
+        let levelArr = [];
+        let currentNode;
+
+        for(let i = 0; i < size; i++) {
+
+            if(level % 2 === 0) {
+                // left to right
+                currentNode = queue.shift();
+                levelArr.push(currentNode.val);
+                if(currentNode.left !== null) {
+                    queue.push(currentNode.left);
+                }
+
+                if(currentNode.right !== null) {
+                    queue.push(currentNode.right);
+                }
+            } else {
+                // right to left
+                currentNode = queue.pop();
+                levelArr.push(currentNode.val);
+                if(currentNode.right !== null) {
+                    queue.unshift(currentNode.right);
+                }
+
+                if(currentNode.left !== null) {
+                    queue.unshift(currentNode.left);
+                }
+            }
+        }
+
+        level++;
+        res.push(levelArr);
+    }
+
+    return res;
+};
