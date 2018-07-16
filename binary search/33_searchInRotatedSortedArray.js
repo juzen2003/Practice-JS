795122d715aa35758de6ef595dda0eea4c593e01
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-16 00:01:09 -0700

    Update 33

---
+++ binary search/33_searchInRotatedSortedArray.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// elements are not duplicated
+var search = function(nums, target) {
+  if(nums.length === 0) return -1;
+
+  let low = 0;
+  let high = nums.length - 1;
+
+  while(low <= high) {
+    let mid = parseInt((low + high) / 2);
+
+    if(nums[mid] === target) return mid;
+
+    if(nums[mid] >= nums[low]) {
+      // left side is sorted
+      // search left if target is at left
+      // else search right
+      if(target >= nums[low] && target < nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    } else {
+      // right side is sorted
+      // search right if target is at right
+      // else search left
+      if(target <= nums[high] && target > nums[mid]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
