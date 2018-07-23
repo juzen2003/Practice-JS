432fbd082aa77c094d2d063559628aa67e25038b
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-23 16:45:37 -0700

    Update 29

--- binary search/29_divideTwoIntegers.js
+++ binary search/29_divideTwoIntegers.js
@@ -6,9 +6,8 @@
 var divide = function(dividend, divisor) {
   let res = 0;
   let positive = true;
-  if((dividend < 0 || divisor < 0) && !(dividend < 0 && divisor < 0)) {
-    positive = false;
-  }
+
+  if((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) positive = false;
 
   let dv = Math.abs(dividend);
   let ds = Math.abs(divisor);
@@ -16,13 +15,12 @@ var divide = function(dividend, divisor) {
   let newDs = ds;
   let incr = 1;
 
-
   while(dv >= ds) {
     dv -= newDs;
     newDs += newDs; // keep adding up newDS like newDs * 2
     res += incr;
     incr += incr; // quotient also * 2 if newDs double
-
+    
     // after newDs double, if it's larger than dividend, we use original divisor
     if(dv < newDs) {
       newDs = ds;
@@ -30,16 +28,15 @@ var divide = function(dividend, divisor) {
     }
   }
 
-  if(positive) {
-    res = res;
-  } else {
-    res = res * (-1);
-  }
+  if(!positive) res = res * -1;
 
-  if(res > Math.pow(2,31) - 1) {
-    res = Math.pow(2,31) - 1;
-  } else if (res < (-1) * Math.pow(2, 31)) {
-    res = (-1) * Math.pow(2, 31);
+  let max = Math.pow(2, 31) - 1;
+  let min = Math.pow(2, 31) * -1;
+
+  if(res > max) {
+    res = max;
+  } else if(res < min) {
+    res = min;
   }
 
   return res;
