ef5aac809b7b8aa6cac1fb5f2e516617612c24e0
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-27 22:59:15 -0700

    Add 104

---
+++ bfs/104_maximumDepthOfBinaryTree.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+// BFS, count all levels
+var maxDepth = function(root) {
+  if(!root) return 0;
+
+  let queue = [];
+  let depth = 0;
+
+  queue.push(root);
+
+  while(queue.length !== 0) {
+    let size = queue.length;
+    let currentNode;
+    depth++;
+
+    for(let i = 0; i < size; i++) {
+      currentNode = queue.shift();
+      if(currentNode.left) queue.push(currentNode.left);
+      if(currentNode.right) queue.push(currentNode.right);
+    }
+  }
+
+  return depth;
+};
+
+// recursion
+// var maxDepth = function(root) {
+//     if(!root) return 0;
+//
+//     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+// };
