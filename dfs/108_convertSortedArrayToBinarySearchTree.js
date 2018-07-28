ec7d4599decab9917ff7234565b1c4865e767585
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-28 16:54:45 -0700

    Add 108

---
+++ dfs/108_convertSortedArrayToBinarySearchTree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+// DFS,
+// an ascending arr of a BST is actually the output of inorder traversal: left, root, right
+// convert an ascending array into a height-balanced BST
+// a height-balanced binary tree: a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+//
+var sortedArrayToBST = function(nums) {
+  let bst = build(nums, 0, nums.length-1);
+  return bst;
+};
+
+const build = function(arr, low, high) {
+  if(low > high) return null;
+
+  let mid = parseInt((low + high)/2);
+  let root = new TreeNode(arr[mid]);
+  root.left = build(arr, low, mid-1);
+  root.right = build(arr, mid+1, high);
+
+  return root;
+};
