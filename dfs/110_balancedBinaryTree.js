19a7a7b4a85da98581405429b5333c2fb524b1ee
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-28 22:38:46 -0700

    Add 110

---
+++ dfs/110_balancedBinaryTree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+// DFS
+// a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+var isBalanced = function(root) {
+  if(!root) return true;
+
+  let left = getDepth(root.left);
+  let right = getDepth(root.right);
+
+  if(Math.abs(left-right) > 1) return false;
+  return isBalanced(root.left) && isBalanced(root.right);
+};
+
+const getDepth = function(root) {
+  if(!root) return 0;
+  return 1 + Math.max(getDepth(root.left), getDepth(root.right));
+};
