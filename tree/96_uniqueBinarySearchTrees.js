61d7d0f3507ce3a09ed818ae6ee3486ddb3945a0
Yu-Jen Chang <juzen2003@gmail.com>
2018-07-31 23:31:42 -0700

    Add 96

---
+++ tree/96_uniqueBinarySearchTrees.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+  if(!n) return 0;
+  // use 2D matrix dp to store the previous calculated result; otherwise memory limited error
+  let dp = [...new Array(n+1)].map(el => new Array(n+1));
+  let count = genTreeCount(1, n, dp);
+  return count;
+};
+
+// generate number of trees from start to end
+const genTreeCount = function(start, end, dp) {
+  if(start > end) return 0;
+  if(dp[start][end]) return dp[start][end];
+  let count = 0;
+
+  for(let i = start; i <= end; i++) {
+    let left = genTreeCount(start, i-1, dp);
+    let right = genTreeCount(i+1, end, dp);
+
+    if(!left && !right) {
+      count += 1;
+    } else if(!left || !right) {
+      count += (left + right);
+    } else {
+      count += (left * right);
+    }
+  }
+
+  dp[start][end] = count;
+  return count;
+};
