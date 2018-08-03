659e87917d28410c2e5fde24a9d677e2eeea8c3b
Yu-Jen Chang <juzen2003@gmail.com>
2018-08-02 22:21:13 -0700

    Add 297

--- tree/236_lowestCommonAncestorOfABinaryTree.js
+++ tree/236_lowestCommonAncestorOfABinaryTree.js
@@ -11,15 +11,16 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
- // All of the nodes' values will be unique.
- // p and q are different and both values will exist in the binary tree.
- // Time O(n), go through all nodes
+// BT
+// All of the nodes' values will be unique.
+// p and q are different and both values will exist in the binary tree.
+// Time O(n), go through all nodes
 
- // for a sub tree rooted at currentNode (full tree starts from root):
- // if currentNode is either p or q, the ancestror is currentNode.
- // else if we found p & q (or common ancestor) at both left & right, then the ancestor is currentNode as well
+// for a sub tree rooted at currentNode (full tree starts from root):
+// if currentNode is either p or q, the ancestror is currentNode.
+// else if we found p & q (or common ancestor) at both left & right, then the ancestor is currentNode as well
 
- // if we only found common ancestor at one side of subtree, the it means p & q are at the same side, that common ancestor at that side is the lowest common ancestor.
+// if we only found common ancestor at one side of subtree, the it means p & q are at the same side, that common ancestor at that side is the lowest common ancestor.
 var lowestCommonAncestor = function(root, p, q) {
   if(!root) return null;
   if(root === p || root === q) return root;
@@ -28,10 +29,13 @@ var lowestCommonAncestor = function(root, p, q) {
   let rightSubLCA = lowestCommonAncestor(root.right, p, q);
 
   if(leftSubLCA && rightSubLCA) {
+    // p/q at both sides
     return root;
   } else if(leftSubLCA) {
+    // p/q at the left side
     return leftSubLCA;
   } else {
+    // p/q at the right side
     return rightSubLCA;
   }
 };
---
+++ tree/297_serializeAndDeserializeBinaryTree.js
@@ -0,0 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+  if(!root) return null;
+
+  let queue = [];
+  let res = [];
+  queue.push(root);
+
+  while(queue.length !== 0) {
+    let currentNode = queue.shift();
+
+    if(!currentNode) {
+      res.push(null);
+    } else {
+      res.push(currentNode.val);
+    }
+
+    if(currentNode) {
+      queue.push(currentNode.left);
+      queue.push(currentNode.right);
+    }
+  }
+
+  while(res[res.length-1] === null) {
+    res.pop();
+  }
+
+  return JSON.stringify(res);
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+  if(!data) return [];
+  let input = JSON.parse(data);
+
+  let root = new TreeNode(input[0]);
+  let count = 1;
+  let queue = [];
+  queue.push(root);
+
+  while(queue.length !== 0) {
+    let currentNode = queue.shift();
+
+    if(input[count] !== undefined && input[count] !== null) {
+      let left = new TreeNode(input[count]);
+      currentNode.left = left;
+      queue.push(left);
+    }
+
+    count++;
+
+    if(input[count] !== undefined && input[count] !== null) {
+      let right = new TreeNode(input[count]);
+      currentNode.right = right;
+      queue.push(right);
+    }
+
+    count++;
+  }
+
+  return root;
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
