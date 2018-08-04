6cceaafff982a159150b0f5a75fe4c7634ddc673
Yu-Jen Chang <juzen2003@gmail.com>
2018-08-03 22:57:39 -0700

    Add 24

--- linked list/24_swapNodesInPairs.js
+++ linked list/24_swapNodesInPairs.js
@@ -9,23 +9,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-// space: O(1) 
+// space: O(1)
 var swapPairs = function(head) {
-    let dummyHead = new ListNode(null);
-    dummyHead.next = head;
-    let current = dummyHead;
+  let dummyHead = new ListNode(null);
+  dummyHead.next = head;
+  let current = dummyHead;
 
+  while(current.next !== null && current.next.next !== null) {
+    let first = current.next;
+    let second = current.next.next;
 
+    first.next = second.next;
+    second.next = current.next; // first
+    current.next = second;
+    current = current.next.next;
+  }
 
-    while(current.next && current.next.next) {
-        let first = current.next;
-        let second = current.next.next;
-
-        first.next = second.next;
-        current.next = second;
-        second.next = first;
-        current = current.next.next;
-    }
-
-    return dummyHead.next;
+  return dummyHead.next;
 };
