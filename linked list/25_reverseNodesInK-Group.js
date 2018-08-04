276750e630a48a22b6240ed0cc4b5676637730c6
Yu-Jen Chang <juzen2003@gmail.com>
2018-08-03 23:45:58 -0700

    Add 25

---
+++ linked list/25_reverseNodesInK-Group.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+// space O(1)
+// time O(n) + O(n) => O(n)
+var reverseKGroup = function(head, k) {
+  if(k === 1) return head;
+
+  let dummyHead = new ListNode(null);
+  dummyHead.next = head;
+  let current = dummyHead;
+
+  let len = 0;
+  while(current.next) {
+    len++;
+    current = current.next;
+  }
+
+  current = dummyHead;
+
+  while(len >= k) {
+    let first = current.next;
+    let second = current.next.next;
+
+    for(let i = 1; i < k; i++) {
+      first.next = second.next;
+      second.next = current.next;
+      current.next = second;
+      second = first.next;
+    }
+
+    current = first;
+    len -= k ;
+  }
+
+  return dummyHead.next;
+};
