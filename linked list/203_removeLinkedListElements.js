cde6ee20a6f6e4dbb1ef30d9b1ea8d3417c282a1
Yu-Jen Chang <juzen2003@gmail.com>
2018-08-12 14:27:29 -0700

    Add 203

---
+++ linked list/203_removeLinkedListElements.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+// Time: O(n)
+// space: O(1)
+var removeElements = function(head, val) {
+  let dummyHead = new ListNode(null);
+  dummyHead.next = head;
+  let prev = dummyHead;
+  let current = head;
+
+  while(current) {
+    while(current && current.val === val) {
+      current = current.next;
+    }
+
+    prev.next = current;
+    prev = prev.next;
+    if(current) current = current.next;
+  }
+
+  return dummyHead.next;
+};
