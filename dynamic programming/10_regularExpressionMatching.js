9911c6bb4f0ba61078827510fb376ebab4566eac
Yu-Jen Chang <juzen2003@gmail.com>
2018-08-15 11:30:30 -0700

    Add 53

--- dynamic programming/10_regularExpressionMatching.js
+++ dynamic programming/10_regularExpressionMatching.js
@@ -5,11 +5,12 @@
  */
 // dp: a 2D array to store the matching result of:
 // s.slice(0, i) (idx: 0 up to i-1) & p.slice(0, j) (idx: 0 up to j-1) in dp[i][j]
+// s.slice(0, i+1) (idx: 0 up to i) & p.slice(0, j+1) (idx: 0 up to j) in dp[i+1][j+1]
 // initialize dp[0][0] = true when there is nothing to check
 var isMatch = function(s, p) {
   let dp = [...Array(s.length+1)].map(el => Array(p.length+1).fill(false));
   dp[0][0] = true;
-  // if s = "", p is sth like "s*", set it to true
+  // if s = "", p is sth like "s*" or "a*x*b*..", set it to true
   for(let j = 0; j < p.length; j++) {
     if(p[j] === "*" && dp[0][j-1]) {
       dp[0][j+1] = true;
