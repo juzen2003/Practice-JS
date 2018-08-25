28f246592c20ef2ff53d24e4edd2226e67a5031a
Yu-Jen Chang <juzen2003@gmail.com>
2018-08-25 04:25:20 -0700

    Add interview q

---
+++ misc/interview/misc_kiwico_cs_082118.js
@@ -0,0 +1,19 @@
+// explain css box model, including:
+// box-sizing
+// display
+// position
+
+// what you think about the company's website and how to improve?
+
+// how to merge mutiple css files into one, explain your thinking process
+
+// sql table join, left, right, outer, and inner join
+
+// write out a sql code with table join
+
+// what are pros & cons between sql and no sql?
+
+// what would happend when you enter an url on the browser?
+
+// what happen when you click a checkout button and it's not re-directed to anywhere, no reaction?
+// how would you debug from there? Using google chrome dev tool
