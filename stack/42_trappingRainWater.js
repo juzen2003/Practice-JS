baff6ca78b525526618a4b6196804777c5744d0e
Yu-Jen Chang <juzen2003@gmail.com>
2018-09-13 11:55:52 -0700

    Update 42

--- stack/42_trappingRainWater.js
+++ stack/42_trappingRainWater.js
@@ -29,3 +29,34 @@ var trap = function(height) {
 
   return maxWater;
 };
+
+// Two pointers
+// Time: O(n)
+// Space: O(1)
+var trap = function(height) {
+  let left = 0;
+  let right = height.length-1;
+  let leftMax = 0;
+  let rightMax = 0;
+  let maxWater = 0;
+
+  while(left <= right) {
+    if(height[left] <= height[right]) {
+      if(height[left] > leftMax) {
+        leftMax = height[left];
+      } else {
+        maxWater += leftMax - height[left];
+      }
+      left++;
+    } else {
+      if(height[right] > rightMax) {
+        rightMax = height[right];
+      } else {
+        maxWater += rightMax - height[right];
+      }
+      right--;
+    }
+  }
+
+  return maxWater;
+};
