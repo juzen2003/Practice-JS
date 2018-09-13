5d326effa7704c0d774cbe0d9aa9273daa68e7b3
Yu-Jen Chang <juzen2003@gmail.com>
2018-09-12 21:00:24 -0700

    Add 41

---
+++ array/41_firstMissingPositive.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 1. put each positive number at the right place, like: 1 at nums[0], 2 at nums[1], etc
+// 2. if we found the first number at idx i: nums[i] !== i+1, then i+1 is the first missing positive number
+// 3. if all numbers in nums exists, then the first missing positive number is nums.length + 1
+// Time: O(n)
+// Space: O(1)
+var firstMissingPositive = function(nums) {
+  let n = nums.length;
+
+  for(let i = 0; i < n; i++) {
+    while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+    }
+  }
+
+  for(let i = 0; i < n; i++) {
+    if(nums[i] !== i+1) return i+1;
+  }
+
+  return n+1;
+};
--- backtracking/40_combinationSumII.js
+++ backtracking/40_combinationSumII.js
@@ -11,23 +11,23 @@
 // use backtracking
 // Time: O(n ^ n)
 var combinationSum2 = function(candidates, target) {
-    candidates.sort((a,b) => a-b);
-    let res = [];
-    backtracking(res, [], candidates, target, 0);
-    return res;
+  candidates.sort((a,b) => a-b);
+  let res = [];
+  backtracking(res, [], candidates, target, 0);
+  return res;
 };
 
 const backtracking = function(res, tmp, arr, remain, start) {
-    if(remain < 0) {
-        return;
-    } else if(remain === 0) {
-        res.push(tmp.slice());
-    } else {
-        for(let i = start; i < arr.length; i++) {
-            if(i > start && arr[i] === arr[i-1]) continue;
-            tmp.push(arr[i]);
-            backtracking(res, tmp, arr, remain-arr[i], i+1);
-            tmp.pop();
-        }
+  if(remain < 0) {
+    return;
+  } else if(remain === 0) {
+    res.push(tmp.slice());
+  } else {
+    for(let i = start; i < arr.length; i++) {
+      if(i > start && arr[i] === arr[i-1]) continue; // avoid duplicated combo
+      tmp.push(arr[i]);
+      backtracking(res, tmp, arr, remain-arr[i], i+1); // i + 1 don't reuse the same element
+      tmp.pop();
     }
+  }
 };
