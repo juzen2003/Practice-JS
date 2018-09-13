9121eb67d98f1dabd2c22032f70428dfa99dd9df
Yu-Jen Chang <juzen2003@gmail.com>
2018-09-13 14:01:06 -0700

    Add 48

---
+++ array/48_rotateImage.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+// clockwise, 90 degree
+// reverse matrix from top to down
+// swap matrix[i][j] with matrix[j][i], swap symmetry
+// Time: O(n^2)
+var rotate = function(matrix) {
+  matrix.reverse();
+
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = i + 1; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+};
+
+// counter clockwise, -90 degree
+// swap matrix[i][j] with matrix[j][i], swap symmetry
+// reverse matrix from top to down
+var antiRotate = function(matrix) {
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = i + 1; j < matrix[0].length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+  
+  matrix.reverse();
+};
