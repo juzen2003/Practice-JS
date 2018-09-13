6817fcac5e45af71a6708f5c4b5136fd907d6c7a
Yu-Jen Chang <juzen2003@gmail.com>
2018-09-13 16:33:09 -0700

    Add 59

--- array/59_spiralMatrixII.js
+++ array/59_spiralMatrixII.js
@@ -1,43 +1,47 @@
 /**
- * @param {number[][]} matrix
- * @return {number[]}
+ * @param {number} n
+ * @return {number[][]}
  */
-// Time: O(n * m), number of grids
-// Space: O(n * m)
-var spiralOrder = function(matrix) {
-  let res = [];
-  if(matrix.length === 0 || matrix[0].length === 0) return res;
+// Time: O(n);
+// Space: O(n ^ 2)
+var generateMatrix = function(n) {
+  let res = [...Array(n)].map(el => Array(n));
 
   let top = 0;
-  let bottom = matrix.length - 1;
+  let bottom = n-1;
   let left = 0;
-  let right = matrix[0].length - 1;
+  let right = n-1;
+  let num = 1;
 
   while(true) {
     // move right
     for(let i = left; i <= right; i++) {
-      res.push(matrix[top][i]);
+      res[top][i] = num;
+      num++;
     }
     top++;
     if(top > bottom || left > right) break;
 
     // move down
     for(let i = top; i <= bottom; i++) {
-      res.push(matrix[i][right]);
+      res[i][right] = num;
+      num++;
     }
     right--;
     if(top > bottom || left > right) break;
 
     // move left
     for(let i = right; i >= left; i--) {
-      res.push(matrix[bottom][i]);
+      res[bottom][i] = num;
+      num++;
     }
     bottom--;
     if(top > bottom || left > right) break;
 
     // move up
     for(let i = bottom; i >= top; i--) {
-      res.push(matrix[i][left]);
+      res[i][left] = num;
+      num++;
     }
     left++;
     if(top > bottom || left > right) break;
