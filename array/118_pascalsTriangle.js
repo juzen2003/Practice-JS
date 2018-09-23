017f07246912762e179e64ef6e42462f23d0c265
Yu-Jen Chang <juzen2003@gmail.com>
2018-09-22 21:56:16 -0700

    Add 118

---
+++ array/118_pascalsTriangle.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+// new row can be calculated from previous row
+// ex:
+//   1 3 3 1 0
+// + 0 1 3 3 1
+//   1 4 6 4 1
+// Time: O(n ^ 2), loop through number of rows O(n) and each loop needs to loop through prev to calculate currentRow O(n)
+var generate = function(numRows) {
+  if(numRows === 0) return [];
+  if(numRows === 1) return [[1]];
+
+  let res = [[1]];
+
+  for(let i = 1; i < numRows; i++) {
+    let prev = [...res[i-1], 0];
+    let shifted = [0, ...res[i-1]];
+    let currentRow = prev.map((el, idx) => el + shifted[idx]);
+    res.push(currentRow);
+  }
+
+  return res;
+};
