3506713bbb3f43041e29149898b3f270bcb43e35
Yu-Jen Chang <juzen2003@gmail.com>
2018-09-26 13:45:06 -0700

    Update misc file monkey river

--- misc/interview/misc_monkeyRiver_092618.js
+++ misc/interview/misc_monkeyRiver_092618.js
@@ -1,90 +1,3 @@
-// const str = 'question';
-//
-// console.log(str.split('').map(char => char.charCodeAt(0)).reduce((memo, c) => memo + c, 0));
-//
-// console.log(113+117+101+115+116+105+111+110);
-
-// let m = new Map();
-// m.set(1,3);
-// m.set(3,5);
-// m.set(7,8);
-// for(let [key, value] of m) {
-//   console.log(key);
-//   console.log(value);
-// }
-//
-// for(let key of m.keys()) {
-//   console.log(key);
-// }
-
-// function test() {
-//   console.log(x);
-// }
-// test();
-// var x = 5;
-
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-// ******
-// start from position -1, end at position N
-// can jump between 1 and D where D is <= N
-// A[K] = T, it means at time T, stone at position K would surface (K: 0 ~ N-1)
-// return earliest time to reach to another bank
-// return 0 if only one jump
-// return -1 if never make it
-// ******
-// 1. Use a hash map to store available stone at each time, key: time, value: available stone
-// 2. loop through keys of hash map and use greedy to see if the monkey and reach to another bank
-// 3.
-// Time:
-// function solution(A, D) {
-//
-//     let N = A.length;
-//     if(-1 + D >= N) return 0;
-//
-//     // build up hash map, key: time, value: stone that pops out
-//     // time: O(n)
-//     let map = new Map();
-//     for(let i = 0; i < N; i++) {
-//       if(A[i] !== -1) map.set(A[i], i);
-//       // map.set(A[i], i);
-//     }
-//     // console.log(map)
-//     // Map { 1 => 0, 0 => 2, 2 => 3, 3 => 4, 5 => 5 }
-//     // build up time array
-//     // time: O(n)
-//     let time = [];
-//     for(let key of map.keys()) {
-//       if(time.length === 0 || key > time[time.length-1]) {
-//         time.push(key);
-//       } else {
-//         time.unshift(key);
-//       }
-//     }
-//     // console.log(time);
-//
-//     // update hash map, key: time, value: stones available at the time
-//     // time: O(n)
-//     for(let i = 0; i < time.length; i++) {
-//       let prev = i === 0 ? [] : map.get(time[i-1]);
-//       map.set(time[i], [...prev, map.get(time[i])]);
-//       map.set(time[i], new Set(map.get(time[i])));
-//     }
-//     console.log(map);
-//     // Map {
-//     //   1 => [ 2, 0 ],
-//     //   0 => [ 2 ],
-//     //   2 => [ 2, 0, 3 ],
-//     //   3 => [ 2, 0, 3, 4 ],
-//     //   5 => [ 2, 0, 3, 4, 5 ] }
-//
-//     for(let i = 0; i < time.length; i++) {
-//
-//     }
-//
-// }
-
-
 // ******
 // start from position -1, end at position N
 // can jump between 1 and D where D is <= N
@@ -127,7 +40,7 @@ function solution1(A, D) {
 //    - store the min time to an array (minArr)
 // 2. loop through A from idx i: 0 ~ N-D, at current location i:
 // 3. keep track of the earliest time the monkey can jump from current location i to a location within i+1 ~ i+D using the data we got from step 1 in minArr
-// Time: O(N) + O(N) ~ O(N) where N is the length of A
+// Time: O(N) + O(N-D) -> O(N) where N is the length of A
 function solution(A, D) {
 
     let N = A.length;
