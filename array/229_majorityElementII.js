30707bb6e71da8a0b8bc096c7956329795aff922
Yu-Jen Chang <juzen2003@gmail.com>
2018-10-02 23:49:55 -0700

    Add 229

---
+++ array/229_majorityElementII.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// Time: O(n)
+var majorityElement = function(nums) {
+  let map = new Map();
+  let res = [];
+  let k = parseInt(nums.length/3);
+
+  for(let i = 0; i < nums.length; i++) {
+    let count = map.get(nums[i]) === undefined ? 1 : map.get(nums[i]) + 1;
+    map.set(nums[i], count);
+  }
+
+  for(let key of map.keys()) {
+    if(map.get(key) > k) res.push(key);
+  }
+
+  return res;
+};
